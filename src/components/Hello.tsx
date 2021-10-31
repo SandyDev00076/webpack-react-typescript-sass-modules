9afcf4ead6fed3ef004485a92813ed7560a7af54
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-11-01 00:19:26 +0530

    add a basic template

---
+++ src/components/Hello.tsx
@@ -0,0 +1,5 @@
+const Hello = () => {
+  return <h1>React Component Starter</h1>;
+};
+
+export default Hello;
---
+++ webpack/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+    resolve: {
+        extensions: ['tsx', 'ts', 'js']
+    },
+    devServer: {
+        port: 3010,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, "..", "./build"),
+        filename: "output.js"
+    },
+    mode: 'development',
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, "..", "./src/index.html")
+        })
+    ],
+    resolve: {
+        fallback: { "querystring": false }
+    }
+}
